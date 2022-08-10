833bbbdf454a2971be5f7c08d6c8e643c8441dca
byMett <58536831+byMett@users.noreply.github.com>
2022-08-10 23:59:57 +0300

    First Commit

---
+++ JavaScript/JsBolum1/js/script.js
@@ -0,0 +1,10 @@
+let firstCity = "Ankara";
+
+       let secondCity = "İstanbul";
+
+       const isEqual = (firstCity === secondCity) ? "İki şehir aynı" : "İki şehir aynı değil";
+
+       textInfo = "isEqual"
+       
+let info = document.querySelector("#info")
+info.innerHTML = '${textInfo}'
